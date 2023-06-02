9cdcd8b858011f4b12f993f6783050a446c4a514
Federico Ferri <federico.ferri.it@gmail.com>
2023-06-02 14:00:57 +0200

    move plugin load to sysCall_init

--- simAddOnLDraw importer.lua
+++ simAddOnLDraw importer.lua
@@ -1,10 +1,9 @@
-simLDraw=require'simLDraw'
-
 function sysCall_info()
     return {autoStart=false,menu='Importers\nLDraw importer...'}
 end
 
 function sysCall_init()
+    simLDraw=require'simLDraw'
     local scenePath=sim.getStringParameter(sim.stringparam_scene_path)
     local filePath=simUI.fileDialog(simUI.filedialog_type.load,'Open LDraw file...',scenePath,'','LDraw files (*.ldr; *.dat; *.mpd)','ldr;dat;mpd',true)
     filePath=filePath[1]
