3fc7968c183ddbfa71121d08ce20bd67bccc3378
Federico Ferri <federico.ferri.it@gmail.com>
2024-06-11 14:21:33 +0200

    simUI.fileDialog fix

--- addOns/LDraw importer.lua
+++ addOns/LDraw importer.lua
@@ -8,11 +8,10 @@ function sysCall_init()
     simUI = require 'simUI'
     simLDraw = require 'simLDraw'
     local scenePath = sim.getStringParam(sim.stringparam_scene_path)
-    local filePath = simUI.fileDialog(
+    local fileNames = simUI.fileDialog(
                          simUI.filedialog_type.load, 'Open LDraw file...', scenePath, '',
                          'LDraw files (*.ldr; *.dat; *.mpd)', 'ldr;dat;mpd', true
                      )
-    filePath = filePath[1]
-    if filePath ~= '' then simLDraw.import(filePath) end
+    if #fileNames > 0 then simLDraw.import(fileNames[1]) end
     return {cmd = 'cleanup'}
 end
